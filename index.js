c1b5653dbecb8a19da0d795a1d15073bbffa8426
thezachcannon <zachary.cannon@hotmail.com>
2018-12-17 12:26:18 -0600

    Updates to Logging and Environment Configuration

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var httpProxy = require("http-proxy");
 var fs = require("fs");
-
-let environment = process.env.stage || "development";
+const chalk = require('chalk');
+const figlet = require('figlet')
+let environment = process.env.NODE_ENV
 let serversJson = fs.readFileSync("./environments/servers.json");
 let servers = JSON.parse(serversJson);
 let proxies = {};
 
-initServers(servers);
+figlet('Proxception', function(err, data) {
+  if (err) {
+      return;
+  }
+  console.log(chalk.cyan(data))
+  initServers(servers)
+});
 
 function initServers(servers) {
   if (servers) {
     let serverNames = Object.keys(servers);
     serverNames.forEach(serverName => {
-      proxies[serverName] = httpProxy
+      if(servers[serverName].enabled){
+        proxies[serverName] = httpProxy
         .createServer({
           ssl: {
             key: fs.readFileSync("./ssl/server.key", "utf8"),
@@ -23,6 +31,11 @@ function initServers(servers) {
           secure: false
         })
         .listen(servers[serverName].port);
+        console.log(`${chalk.blueBright(serverName)} : ${chalk.greenBright('RUNNING')}`)
+      }
+      else{
+        console.log(`${chalk.blueBright(serverName)} : ${chalk.redBright('NOT ENABLED')}`)
+      }
     });
   }
 }
